4e218627f98e335820a6ff414867eafb945e6b1c
Awesome <awesome_devnet@outlook.com>
2024-12-06 13:30:07 +0900

    라우트 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Main from "./page/main/main";
 import About from "./page/about/about";
